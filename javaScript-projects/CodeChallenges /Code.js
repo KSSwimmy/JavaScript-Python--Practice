d36f1981c2c953ad98bc92029758a880eedc62dc
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-02-09 02:45:38 -0500

    :wastebasket: Filtering out friends
    Filtering out friends

--- javaScript-projects/CodeChallenges /Code.js
+++ javaScript-projects/CodeChallenges /Code.js
@@ -15,11 +15,14 @@ var countBits = function(n) {
       console.log(conversion) 
     } //step 3: return the count.
       return count
-   
+
   };
-  
+
 ///////// ANOTHER SOLUTION /////////////////////////////////
 countBits = n => n.toString(2).split('0').join('').length;
+console.log(n.toString(2))
+console.log(split('0'))
+console.log(join('').length)
 
 
 /* 2. Trolls are attacking your comment section!
@@ -34,9 +37,67 @@ function disemvowel(str) {
     let stringy = ["This website is for losers LOL!"];
     
     str = stringy.replace(/^[aeiou]$/i, ""); 
-     
-      return str;
-      
+    console.log(str)
+    return str;
+
     }
+
+/* 3. Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.*/
+
+function friend(friends){
+    //assign a var to an array of names
+    
+    //filter the list of names
+    //if the names have 4 letters in it choose that one
+    //if not then get rid of it
+    const removeFriend = friends.filter(name => name.length === 4);
+    
+   //returns friend's name
+    return removeFriend
+}
+    
+///////// ANOTHER SOLUTION /////////////////////////////////
+function friend(friends){
+    // defined an empty array
+    const myFriends = []
+    // assigned number of friends to (friends) to get the length of friends
+    const numberOfFriends = friends.length
+    // loops through number of friends
+    for (let i = 0; i < numberOfFriends; i++) {
+      // assigned friend name to 
+      const friendName = friends[i]
+      // assigned isFriend to the variable friendName (friends[i]) that is the length of 4 
+      const isFriend = (friendName.length === 4)
+      if (isFriend) myFriends.push(friendName)
+      console.log('This is const friendName',friends[i]) 
+    }
+    return myFriends
+  }
+  
+///////// ANOTHER ANOTHER SOLUTION /////////////////////////////////
+
+function friend(friends){
+    //your code here
+    var realFriends = [];
+    for(i=0; i<friends.length; i++){
+      if(friends[i].length == 4 && isNaN(friends[i])){
+        realFriends.push(friends[i]);
+      }
+    }
+    
+    return realFriends
+  }
+
+/* Test.assertSimilar(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"]);
+Test.assertSimilar(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"]);
+Test.assertSimilar(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"]);
+Test.assertSimilar(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"]); */
 
 
