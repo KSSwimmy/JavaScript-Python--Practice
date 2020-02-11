eb3c1f447c3ae3b67a115f5bbcf5738d21ecb1e0
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-02-11 17:54:51 -0500

    :sparkles: Added python code challenges
    Added python code challenges

--- javaScript-projects/CodeChallenges /Code.js
+++ javaScript-projects/CodeChallenges /Code.js
@@ -159,3 +159,86 @@ function greeting(parameterVariable) {
 parameterVariable: 'a string'
         console.log(parameterVariable)
 }
+
+
+//REVERSE STRING /////////////////////////////////////////////////////////////////////////////////////////////
+
+function reverseString(str) {
+    // Step1: use the .split() method to turn the string into an array of strings. 
+    //console.log(str.split("")) // will look like "h" "e" "l" "l" "o" etc..
+    
+    
+    let splitString = str.split('');
+    
+    // Step2: use the .reverse() method. Which reverses an array in place. 
+    //Each individual string of letters will be reversed since we used the split method.
+    
+    let reversedString = splitString.reverse();
+    
+    // Step3: use the .join() method to crate a new string by joining the array together.
+    
+    let joinString = reversedString.join('');
+    
+    // Step4: Return joinString 
+    
+    return joinString;
+    
+  }
+  
+  //Invoke the 'reverseString'
+  
+  reverseString('hello world')
+
+  //OTHER SOLUTIONS //////////////////////////////////////////////////
+
+  // v1.0
+function reverseString(str) {
+    let len = str.length;
+    let reversed = '';
+    for (let i = len; i > 0; i--) {
+      reversed += str[i - 1];
+    }
+    return reversed;
+  }
+  
+  // // v1.1
+  // function reverseString(str) {
+  //   let len = str.length;
+  //   let reversed = '';
+  //   for (let i = len - 1; i >= 0; i--) {
+  //     reversed += str[i];
+  //   }
+  //   return reversed;
+  // }
+  
+  // // v2.0 using an array
+  // function reverseString(str) {
+  //   let len = str.length;
+  //   let arr = [];
+  //   for (let i = len; i > 0; i--) {
+  //     arr.push(str[i - 1]);
+  //   }
+  //   let reversed = '';
+  //   for (let j = 0; j < arr.length; j++) {
+  //     reversed += arr[j];  
+  //   }
+  //   return reversed;
+  // }
+  
+  // // v2.2 using JavaScript methods
+  // function reverseString(str) {
+  //   let split =  str.split('');
+  //   let reversed = split.reverse();
+  //   let joined = reversed.join('');
+  //   return joined;
+  // }
+  
+  // // v2.3 dot chaining methods & returning their evaluation
+  // function reverseString(str) {
+  //   return str.split('').reverse().join('');
+  // }
+  
+  // // v2.4 ES6 style
+  // const reverseString = (str) => {
+  //   return str.split('').reverse().join('');
+  // };
---
+++ javaScript-projects/CodeChallenges /Code.py
@@ -0,0 +1,46 @@
+#REVERSE A STRING /////////////////////////////////////////////////////////////////////////
+
+# string = 'Hello Word' [::-1]
+# print(string)
+
+#other solutions //////////////////
+
+#Make a function
+# def this_function(backwards):
+#     return backwards[::-1]
+# theOtherSting = this_function('This is what the string looks like backwards')
+# print(theOtherSting)
+
+#REVERSE A NUMBER ///////////////////////////////////////////////////////////////////////////
+# num = 4562
+# reverse = 0
+
+# while (num > 0):
+#     remainder = num % 10
+#     reverse = reverse * 10 + remainder
+#     num = num / 10
+# print(reverse) 
+# FAILED ATTEMPT 
+
+# Python 3 program to reverse digits 
+# of a number 
+rev_num = 0
+base_pos = 1
+  
+# Recursive function to reverse 
+# digits of num 
+def reversDigits(num): 
+    global rev_num 
+    global base_pos 
+    if(num > 0): 
+        reversDigits((int)(num / 10)) 
+        rev_num += (num % 10) * base_pos 
+        base_pos *= 10
+    return rev_num 
+      
+# Driver Code  
+num = 4562
+print("Reverse of no. is ", 
+         reversDigits(num)) 
+
+#//////////////////////////////////////////////////////////////////////////////////////
