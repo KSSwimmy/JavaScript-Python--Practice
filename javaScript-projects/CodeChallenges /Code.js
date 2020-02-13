8304a36eee25558f7e364eba4f7b1d25dbb1a211
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-02-12 22:08:37 -0500

    :pencil: Starting ToCamelCasing
    Starting ToCamelCasing

--- javaScript-projects/CodeChallenges /Code.js
+++ javaScript-projects/CodeChallenges /Code.js
@@ -258,13 +258,14 @@ function reverseString(str) {
     
     }
     reverseNumber(12345);
+
+
     
 //REVERSE CASING///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Write a function that takes in a string, reverses the 'casing' of that string, 
 //and returns the "reversed-casing" string.
 
 //NEEDS REVISING ******************************
-
 function reverseCase(str) {
     let string = 'HELLO world!'
     
@@ -282,4 +283,12 @@ function reverseCase(str) {
     //console.log()
   
 }
-//NEEDS REVISING ******************************
+//NEEDS REVISING ****************************** works but doesn't pass "reverse_casing" test
+
+
+
+//TO CAMEL CASING///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Good morning! Complete the function so that it converts dash-delimited ("kebab" case) & underscore-delimited 
+//("snake" case) words into "camel" casing. 
+//The first word within the output should be capitalized only if the original word was capitalized.
+
