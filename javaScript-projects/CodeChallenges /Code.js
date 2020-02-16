a838414f343099626d1db6105484e447076edb9d
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-02-16 17:27:54 -0500

    :wrench: Minor change to code.js
    Minor change to code.js

--- javaScript-projects/CodeChallenges /Code.js
+++ javaScript-projects/CodeChallenges /Code.js
@@ -326,5 +326,5 @@ fizzBuzz(1)
 //TO CAMEL CASING///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Good morning! Complete the function so that it converts dash-delimited ("kebab" case) & underscore-delimited 
 //("snake" case) words into "camel" casing. 
-//The first word within the output should be capitalized only if the original word was capitalized.
+//The first word within the output should be capitalized only  if the original word was capitalized.
 
