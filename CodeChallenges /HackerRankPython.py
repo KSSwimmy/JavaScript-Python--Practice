901f7484f17824bb5dc121cb658a78ba102e2302
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-02-26 22:03:36 -0500

    :iphone: HackerRack | Telephone Words
    HackerRack | Telephone Words

--- CodeChallenges /HackerRankPython.py
+++ CodeChallenges /HackerRankPython.py
@@ -166,11 +166,8 @@ def truckTour(petrolpumps):
  # 
  # Frequency Queries Part 1 
  # 
-<<<<<<< HEAD
 
  
-=======
->>>>>>> 3804e133e52282d52d0a6e69377cbc8eb9bc5879
  #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -313,5 +310,45 @@ def superDigit(n, k):
 
 
 
-#
+#Telephone Words 
+
+digits_to_letters = {
+  0: '0',
+  1: '1',
+  2: 'ABC',
+  3: 'DEF',
+  4: 'GHI',
+  5: 'JKL',
+  6: 'MNO',
+  7: 'PQRS',
+  8: 'TUV',
+  9: 'WXYZ'
+}
+
+# what's the runtime? 
+def telephone_words(digits):
+  words = []
+  # how do we call our tw function appropriately?
+  # how do we get the word that we're building up into the words array?  
+  tw('', digits, words)
+  return words
+
+def tw(word_so_far, digits, words):
+  # base case: len(digits) == 0
+  if len(digits) == 0:
+    words.append(word_so_far)
+    return
+  # otherwise, get the next digit 
+  next_digit = int(digits[0])
+  # access its letters from our map 
+  letters = digits_to_letters[next_digit]
+  # loop through all the letters 
+  for l in letters:
+    # we're not storing these answers anywhere 
+    tw(word_so_far + l, digits[1:], words)
+
+
+print(telephone_words('130'))
+
+
 
